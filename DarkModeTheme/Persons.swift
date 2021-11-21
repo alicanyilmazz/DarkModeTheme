9bd2f5fdf5a001353a06f8ee8643bf9fb8bc02f0
alicanyilmazz <alicanyilmaz101@gmail.com>
2021-11-21 17:22:34 +0300

    Person entity was created.

---
+++ DarkModeTheme/Persons.swift
@@ -0,0 +1,22 @@
+//
+//  Persons.swift
+//  DarkModeTheme
+//
+//  Created by alican on 20.11.2021.
+//
+
+import Foundation
+
+class Persons{
+    var personName : String?
+    var personAge : Int?
+    
+    init(){
+        
+    }
+    
+    init(personName : String , personAge : Int){
+        self.personName = personName
+        self.personAge = personAge
+    } 
+}
